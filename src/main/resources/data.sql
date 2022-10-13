f6ea45a44568063a1251842724494e0289deb723
Israel Herrera Cabello <isra.herrera@hotmail.com>
2022-10-14 00:13:45 +0200

    Creacion de Data.sql y Schema.sql con la tabla Productos

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into Producto(nombre,precio,imagen,propietario)values('Bicicleta de montaña', 100.0,'https://www.decathlon.es/media/835/8350582/big_23c25284-2810-415d-8bcc-e6bebdb536fc.jpg', usuario);
+insert into Producto(nombre,precio,imagen,propietario)values('Golf GTI Serie 2', 2500.0,'https://www.minicar.es/large/Volkswagen-Golf-GTi-G60-Serie-II-%281990%29-Norev-1%3A18-i22889.jpg', usuario);
+insert into Producto(nombre,precio,imagen,propietario)values('Raqueta de tenis', 10.5, 'https://imgredirect.milanuncios.com/fg/2311/04/tenis/Raqueta-tenis-de-segunda-mano-en-Madrid-231104755_1.jpg?VersionId=T9dPhTf.3ZWiAFjnB7CvGKsvbdfPLHht', usuario);
+insert into Producto(nombre,precio,imagen,propietario)values('Xbox One X', 425.0, 'https://images.vibbo.com/635x476/860/86038583196.jpg', usuario2);
+insert into Producto(nombre,precio,imagen,propietario)values('Trípode flexible', 10.0, 'https://images.vibbo.com/635x476/860/86074256163.jpg', usuario2);
+insert into Producto(nombre,precio,imagen,propietario)values('Iphone 7 128 GB', 350.0, 'https://store.storeimages.cdn-apple.com/4667/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/rosegold/iphone7-rosegold-select-2016?wid=470&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430205982', usuario2);
+			
