b5532e623acd81d15f2f58e713d2358b563511f1
Israel Herrera Cabello <isra.herrera@hotmail.com>
2022-10-14 13:26:57 +0200

    Agregamos parametros al repositorio de busqueda de Productos

--- src/main/java/com/secondhandmarket/spring/repositorio/ProductoRepository.java
+++ src/main/java/com/secondhandmarket/spring/repositorio/ProductoRepository.java
@@ -4,18 +4,20 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.secondhandmarket.spring.entidades.Compra;
 import com.secondhandmarket.spring.entidades.Producto;
+import com.secondhandmarket.spring.entidades.Usuario;
 
 public interface ProductoRepository extends JpaRepository<Producto, Long	>{
 	
 	// Todos los productos de los cuales somos propietarios
-	List<Producto> findByPropietario();
+	List<Producto> findByPropietario(Usuario propietario);
 	//Todos los productos de una compra
-	List<Producto> findByCompra();
+	List<Producto> findByCompra(Compra compra);
 	// Todos los productos que esten todavia en venta y no hayamos comprado
 	List<Producto> findByCompraIsNull();
 	//Producto por nombre que no hayamos comprado
-	List<Producto> findByNombreContainsIgnoreCaseAndCompraIsNull();
+	List<Producto> findByNombreContainsIgnoreCaseAndCompraIsNull(String nombre);
 	//Producto por nombre que hayamos comprado
-	List<Producto> findByNombreContainsIgnoreCaseAndPropietario();
+	List<Producto> findByNombreContainsIgnoreCaseAndPropietario(String nombre, Usuario propietario);
 }
