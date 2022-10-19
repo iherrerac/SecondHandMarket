59dac16d66411f864b93893c496dab65e2e94a4e
Israel Herrera Cabello <isra.herrera@hotmail.com>
2022-10-20 00:52:11 +0200

    Creamos en el controllador de producto la llamada a mis productos y desde alli a poder eliminar un producto qu eno este vendido

--- src/main/java/com/secondhandmarket/spring/controladores/ProductosController.java
+++ src/main/java/com/secondhandmarket/spring/controladores/ProductosController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.secondhandmarket.spring.entidades.Producto;
 import com.secondhandmarket.spring.entidades.Usuario;
@@ -13,6 +18,7 @@ import com.secondhandmarket.spring.servicio.ProductoServicio;
 import com.secondhandmarket.spring.servicio.UsuarioServicio;
 
 @Controller
+@RequestMapping("/app")
 public class ProductosController {
 	
 	@Autowired
@@ -23,10 +29,35 @@ public class ProductosController {
 
 	private Usuario usuario;
 	
-	@ModelAttribute("misProductos")
+	//TEner siempre disponible mis productos
+	@ModelAttribute("misproductos")
 	public List<Producto> misProductos(){
 		String email = SecurityContextHolder.getContext().getAuthentication().getName();
 		usuario = usuarioServicio.buscarPorEmail(email);
 		return (productoServicio.productosDeUnPropietario(usuario));
 	}
+	
+	/*
+	 * Posibilidad de hacer consultas. SI la consulta esta vacia Diretamente tiraremos del modelo "misProductos",
+	 *  si no mostraremos el producto buscado
+	 */
+	@GetMapping("/misproductos")
+	public String listaMisProductos(Model model, @RequestParam(name = "q", required = false) String query) {
+		if (query != null) model.addAttribute("misproductos", productoServicio.buscarMisProductos(query, usuario)); 
+		return "/app/producto/lista";
+	}
+	
+	/*
+	 * Eliminar uno de mis productos: le pasamos el id, lo recogeria, buscaria el producto completo, 
+	 * si no esta comprado lo eliminaria, y nos redirigiria a mis productos 
+	 */
+	
+	@GetMapping("/misproductos/{id}/eliminar")
+	public String eliminar(Model model, @PathVariable Long id) {
+		Producto p = productoServicio.findById(id);
+		if (p.getCompra() == null) productoServicio.borrar(p);  
+		return "redirect:/app/misproductos";
+	}
+	
+	
 }
