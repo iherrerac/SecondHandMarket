8cca22de0a2c36c61824722be70e2dea4e97d9ea
Israel Herrera Cabello <isra.herrera@hotmail.com>
2022-10-20 11:32:36 +0200

    Activamos la configuracion del servicio de ficheros con @EnableConfigurationProperties en la clase de configuracion ConfiguracionAuditoria

--- src/main/java/com/secondhandmarket/spring/configuracion/ConfiguracionAuditoria.java
+++ src/main/java/com/secondhandmarket/spring/configuracion/ConfiguracionAuditoria.java
@@ -1,7 +1,10 @@
 package com.secondhandmarket.spring.configuracion;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.secondhandmarket.spring.upload.StorageProperties;
 
 @Configuration
 @EnableJpaAuditing
@@ -11,7 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  * se le insertará la marca de tiempo del sistema automaticamente con el @EnableJpaAuditing
  * 
  */
-
+@EnableConfigurationProperties(StorageProperties.class) // Habilitamos la configuracion para el almacenamiento
 public class ConfiguracionAuditoria {
 
 }
